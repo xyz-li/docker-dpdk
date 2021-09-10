59acd6f471bc073484ee033688ca9676e2d11f29
LiHui <andrewli@kubesphere.io>
2021-09-10 15:00:59 +0800

    build for ubuntu

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
-FROM registry.access.redhat.com/rhel7/rhel-tools
-MAINTAINER jeder@redhat.com
+FROM ubuntu
 
 LABEL RUN docker run -it --privileged -v /sys/bus/pci/devices:/sys/bus/pci/devices -v /sys/kernel/mm/hugepages:/sys/kernel/mm/hugepages -v /sys/devices/system/node:/sys/devices/system/node -v /dev:/dev --name NAME -e NAME=NAME -e IMAGE=IMAGE IMAGE
 
 # Setup yum repos, or use subscription-manager
 
 # Install DPDK support packages.
-RUN yum install -y sudo libhugetlbfs-utils libpcap-devel \
-    kernel kernel-devel kernel-headers
+RUN apt-get update && DEBIAN_FRONTEND=noninteractiv apt-get  install -y libhugetlbfs-dev  libpcap-dev curl gcc xz-utils meson ninja-build python3-pyelftools iproute2 make pkg-config
 
 # Build DPDK and pktgen-dpdk for x86_64-native-linuxapp-gcc.
 WORKDIR /root
@@ -15,5 +13,6 @@ COPY ./build_dpdk.sh /root/build_dpdk.sh
 COPY ./dpdk-profile.sh /etc/profile.d/
 RUN /root/build_dpdk.sh
 
+ENV  LD_LIBRARY_PATH /usr/local/lib64
 # Defaults to a bash shell, you could put your DPDK-based application here.
 CMD ["/usr/bin/bash"]
--- build_dpdk.sh
+++ build_dpdk.sh
@@ -4,7 +4,7 @@
 #
 #  build_dpdk.sh
 #
-#             - Build DPDK and pktgen-dpdk for 
+#             - Build DPDK and pktgen-dpdk for
 #
 #  Usage:     Adjust variables below before running, if necessary.
 #
@@ -17,33 +17,23 @@
 #  Define Global Variables and Functions
 ################################################################################
 
-URL=http://www.dpdk.org/browse/dpdk/snapshot/dpdk-2.0.0.tar.gz
 BASEDIR=/root
-VERSION=2.0.0
+VERSION=21.08
 PACKAGE=dpdk
+URL=http://fast.dpdk.org/rel/dpdk-${VERSION}.tar.xz
 DPDKROOT=$BASEDIR/$PACKAGE-$VERSION
 CONFIG=x86_64-native-linuxapp-gcc
 
 
 # Download/Build DPDK
 cd $BASEDIR
-curl $URL | tar xz
+curl -LO $URL
+tar xf dpdk-${VERSION}.tar.xz
 cd $DPDKROOT
-make config T=$CONFIG
-sed -ri 's,(PMD_PCAP=).*,\1y,' build/.config
-make config T=$CONFIG install
+meson build
+ninja -C build
 
-# Download/Build pktgen-dpdk
-URL=http://www.dpdk.org/browse/apps/pktgen-dpdk/snapshot/pktgen-dpdk-pktgen-2.8.5-2.tar.xz
-BASEDIR=/root
-VERSION=2.8.5-2
-PACKAGE=pktgen
-PKTGENROOT=$BASEDIR/$PACKAGE-$VERSION
-cd $BASEDIR
-curl $URL | tar xz
 
-# Silence compiler info message
-sed -i '/Wwrite-strings$/ s/$/ -Wno-unused-but-set-variable/' $DPDKROOT/mk/toolchain/gcc/rte.vars.mk
-cd $PKTGENROOT
-make
-ln -s $PKTGENROOT/app/app/$CONFIG/pktgen /usr/bin
+# install
+
+cd build && meson install  && cp meson-private/libdpdk-libs.pc meson-private/libdpdk.pc /usr/lib/x86_64-linux-gnu/pkgconfig/
